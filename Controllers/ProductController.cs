ad108e004c612caa78668a104fd4b25acb4cc61f
chhetribishal <bishal.in.thuma012@gmail.com>
2022-07-18 21:33:41 +0545

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,183 @@
+﻿using ADO_Example.DAL;
+using ADO_Example.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ADO_Example.Controllers
+{
+    public class ProductController : Controller
+    {
+        Product_DAL _productDAL = new Product_DAL();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+           var productList = _productDAL.GetAllProducts();
+            if (productList.Count == 0)
+            {
+                TempData["InfoMessage"] = "Currently Products Are Not In Stock";
+            }
+            return View(productList);
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            try
+            {
+                var product = _productDAL.GetAllProductByID(id).FirstOrDefault();
+                if (product == null)
+                {
+                    TempData["InfoMessage"] = "Product not available with id " + id.ToString();
+                    return RedirectToAction("Index");
+                }
+                return View(product);
+            }
+            catch (Exception ex)
+            {
+                TempData["ErroMessage"] = ex.Message;
+                return View();
+            }
+
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+
+            bool IsInserted = false;
+            try
+            {
+                // TODO: Add insert logic here
+                if (ModelState.IsValid)
+                {
+                    IsInserted = _productDAL.InsertProduct(product);
+                    if (IsInserted)
+                    {
+                        TempData["SuccessMessage"] = "Product Detatil Saved Successfully...!";
+                    }
+                    else
+                    {
+                        TempData["ErrorMessage"] = "Product is already available/Unable to Saved Product Details...!";
+                    }
+
+                   
+                }
+                return RedirectToAction("Index");
+            }
+            catch(Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+                return View();
+            }
+        
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                var products = _productDAL.GetAllProductByID(id).FirstOrDefault();
+                if (products == null)
+                {
+                    TempData["InfoMessage"] = "Product not available with ID " + id.ToString();
+                    return RedirectToAction("Index");
+                }
+                return View(products);
+            }
+            catch(Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+                return RedirectToAction("View");
+            }
+
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost,ActionName("Edit")]
+        public ActionResult UpdateProduct(Product product)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                if (ModelState.IsValid)
+                {
+                    bool IsUpdated = _productDAL.UpdateProduct(product);
+                    if (IsUpdated)
+                    {
+                        TempData["SuccessMessage"] = "Product Details Updated Successfully";
+                    }
+                    else
+                    {
+                        TempData["ErrorMessage"] = "Product is already availabe/unable to update the product details";
+
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            catch(Exception ex)
+            {
+                TempData["ErroMessage"] = ex.Message;
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var product = _productDAL.GetAllProductByID(id).FirstOrDefault();
+                if (product == null)
+                {
+                    TempData["InfoMessage"] = "Product not available with id " + id.ToString();
+                    return RedirectToAction("Index");
+                }
+                return View(product);
+            }
+            catch(Exception ex)
+            {
+                TempData["ErroMessage"] = ex.Message;
+                return View();
+            }
+           
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost,ActionName("Delete")]
+        public ActionResult DeleteConfirmation(int id)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                string result = _productDAL.DeleteProduct(id);
+                if (result.Contains("deleted"))
+                {
+                    TempData["SuccessMessage"] = result;
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = result;
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch(Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+                return View();
+            }
+        }
+    }
+}
